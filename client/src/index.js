bc165cd05cd4098e4993ebd258357ebe335f8368
Charbarwinston <charlesneff13@gmail.com>
2020-07-09 19:47:55 -0500

    more test lines

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
-import Customer from './components/customers/customers';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counter />
-    <Customer />
   </React.StrictMode>,
   document.getElementById('root')
 );
