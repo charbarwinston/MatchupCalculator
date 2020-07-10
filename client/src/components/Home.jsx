59b803ed33086406d71fc6bbd923fba82a9935b3
Zachary Wright <wrighzaa@uni.edu>
2020-07-09 20:16:14 -0500

    Create matchup form

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,7 +5,23 @@ const Home = (props) => {
   return (
     <div className="home">
       <h1>LoL Matchup Calculator</h1>
-      <p>This is the homepage.</p>
+
+      <form action="">
+        <div className="form-row">
+          <div className="form-group col-6">
+            <label htmlFor="">Your Champion</label>
+            <input className="form-control" type="text"/>
+          </div>
+
+          <div className="form-group col-6">
+            <label htmlFor="">Opponent's Champion</label>
+            <input className="form-control" type="text"/>
+          </div>
+        </div>
+        <div className="form-row">
+          <input className="btn btn-primary" type="submit" value="Go"/>
+        </div>
+      </form>
     </div> 
   );
 }
