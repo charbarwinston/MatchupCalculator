f6639c217ed505d58594c62110cfe3805a06c18b
Zachary Wright <wrighzaa@uni.edu>
2020-07-09 20:53:44 -0500

    Create Matchup component; Add champion icons

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,26 +1,39 @@
 import React from 'react';
+import Matchup from './Matchup';
 
 const Home = (props) => {
   return (
     <div className="home">
-      <h1>LoL Matchup Calculator</h1>
-
-      <form action="">
-        <div className="form-row">
-          <div className="form-group col-6">
-            <label htmlFor="">Your Champion</label>
-            <input className="form-control" type="text"/>
-          </div>
-
-          <div className="form-group col-6">
-            <label htmlFor="">Opponent's Champion</label>
-            <input className="form-control" type="text"/>
-          </div>
+      <div className="row">
+        <div className="col-12">
+          <h1>LoL Matchup Calculator</h1>
         </div>
-        <div className="form-row">
-          <input className="btn btn-primary" type="submit" value="Go"/>
+      </div>
+      
+      <div className="row">
+        <div className="col-12">
+          <form action="">
+            <div className="form-row">
+              <div className="form-group col-6">
+                <label htmlFor="">Your Champion</label>
+                <input className="form-control" type="text"/>
+              </div>
+
+              <div className="form-group col-6">
+                <label htmlFor="">Opponent's Champion</label>
+                <input className="form-control" type="text"/>
+              </div>
+            </div>
+            <div className="form-row">
+              <div className="form-group col-6">
+                <input className="btn btn-primary" type="submit" value="Go"/>
+              </div>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
+
+      <Matchup />
     </div> 
   );
 };
--- client/src/components/Matchup.jsx
+++ client/src/components/Matchup.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { useState } from 'react';
 
-const Matchup = () => {
+const Matchup = (props) => {
+  const [userChampion, setUserChampion] = useState('Teemo');
+  const [opponentChampion, setOpponentChampion] = useState('Garen');
 
+  return (
+    <div className="matchup card">
+      <div className="card-header">
+        <h2>The Matchup</h2>
+      </div>
+      <div className="card-body">
+        <div className="row">
+          <div className="col-6">
+            <p>Your Champion: {userChampion}</p>
+            <img src="./images/teemo.png" alt=""/>
+          </div>
+          <div className="col-6">
+            <p>Opponent's Champion: {opponentChampion}</p>
+            <img src="./images/garen.png" alt=""/>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  );
 };
 
 export default Matchup;
